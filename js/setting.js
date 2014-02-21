b240ba96a327b31ef12211810292f383b5c67390
青森大学オープンデータ勉強会 <aomori.univ.open.data@gmail.com>
2014-02-21 15:08:04 +0900

    atai

--- js/setting.js
+++ js/setting.js
@@ -5,7 +5,7 @@
 var Taxes = Taxes || {};
 
 Taxes.baseKoujo = 330000; // 住民税基礎控除
-Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.huyoKoujo = 430000; // 一人分の扶養控除
 Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
